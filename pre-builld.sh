91fc3665548eae944caaf9806a0fa554d791c4eb
xaxaaxaaa-hash <xaxaaxaaa@gmail.com>
2026-05-27 00:30:52 +0500

    Update pre-builld.sh

--- pre-builld.sh
+++ pre-builld.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+set -e
 
-# Отключаем сборку сломанного busybox 1.37.0
-echo ">>> FIX: Force using busybox 1.29.3"
-# Удаляем исходники проблемной версии
+echo ">>> FIX: Replacing broken busybox 1.37.0 with stable 1.29.3"
+
+# Удаляем проблемную версию
 rm -rf trunk/user/busybox/busybox-1.37.0
-# Заменяем их на исходники стабильной версии 1.29.3
-git checkout origin/master -- trunk/user/busybox
+
+# Скачиваем стабильную версию из репозитория
+cd trunk/user/busybox
+wget https://gitlab.com/dm38/padavan-ng/-/raw/master/trunk/user/busybox/busybox-1.29.3.tar.bz2
+tar -xjf busybox-1.29.3.tar.bz2
+ln -sf busybox-1.29.3 busybox-1.29.3
+rm -f busybox-1.29.3.tar.bz2
+cd ../../..
+
+echo ">>> FIX: Busybox replaced successfully"
