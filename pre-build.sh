8240d31f81d8e9b0c1f5843d2055bddc1c8a7185
xaxaaxaaa-hash <xaxaaxaaa@gmail.com>
2026-05-27 01:04:18 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -3,34 +3,44 @@ set -e
 
 echo ">>> FIX: Starting BusyBox replacement routine..."
 
-# 1. Определяем путь к папке с BusyBox
+# Ждём, пока появятся исходники (максимум 60 секунд)
+for i in {1..60}; do
+    if [ -d "trunk/user/busybox" ]; then
+        break
+    fi
+    sleep 1
+done
+
+# Проверяем, что папка существует
+if [ ! -d "trunk/user/busybox" ]; then
+    echo ">>> ERROR: trunk/user/busybox not found after waiting"
+    exit 1
+fi
+
 BUSYBOX_DIR="trunk/user/busybox"
 
-# 2. Удаляем папку со сломанной версией 1.37.0
+# Удаляем сломанную версию
 if [ -d "$BUSYBOX_DIR/busybox-1.37.0" ]; then
     echo ">>> FIX: Removing broken busybox-1.37.0..."
     rm -rf "$BUSYBOX_DIR/busybox-1.37.0"
 fi
 
-# 3. Проверяем, есть ли уже версия 1.29.3
+# Скачиваем стабильную версию, если её нет
 if [ ! -d "$BUSYBOX_DIR/busybox-1.29.3" ]; then
-    echo ">>> FIX: Downloading stable busybox-1.29.3 directly..."
-    # Скачиваем архив напрямую (самый надёжный способ)
+    echo ">>> FIX: Downloading stable busybox-1.29.3..."
     cd "$BUSYBOX_DIR"
-    wget https://gitlab.com/dm38/padavan-ng/-/archive/master/padavan-ng-master.tar.bz2
-    tar -xjf padavan-ng-master.tar.bz2
-    # Копируем только нужную папку
-    cp -r padavan-ng-master/trunk/user/busybox/busybox-1.29.3 .
+    wget -q https://gitlab.com/dm38/padavan-ng/-/archive/master/padavan-ng-master.tar.bz2
+    tar -xjf padavan-ng-master.tar.bz2 2>/dev/null
+    cp -r padavan-ng-master/trunk/user/busybox/busybox-1.29.3 . 2>/dev/null
     rm -rf padavan-ng-master.tar.bz2 padavan-ng-master
     cd ../..
-    echo ">>> FIX: Successfully downloaded and copied busybox-1.29.3."
+    echo ">>> FIX: Successfully downloaded busybox-1.29.3"
 fi
 
-# 4. Создаём символическую ссылку
-echo ">>> FIX: Creating symlink to force using busybox-1.29.3..."
+# Создаём ссылку, обманывая сборщик
 cd "$BUSYBOX_DIR"
 rm -f busybox-1.37.0
 ln -sf busybox-1.29.3 busybox-1.37.0
 cd - > /dev/null
 
-echo ">>> FIX: Busybox replacement completed successfully."
+echo ">>> FIX: Busybox replacement completed successfully"
